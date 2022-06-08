7851b597314948f7b409b893e9c9de01ccd982c5
LLKOS <107107053+LLKOS@users.noreply.github.com>
2022-06-08 11:47:13 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,58 @@
+module.exports = {
+    PREFIX: "!", //prefix for the bot here
+    owner: {
+      id: "952640526744711210",  //put your owner Id in here so you can bypass cooldown for the generator commands and use the nitrogen command
+      username: "plugz#7397"
+    },
+
+    channels: {
+      generator: '969665544305385532', //ID of the generator channel
+      nitro: '966394209596747837', //ID of the nitro generator channel
+      twitch: '969665481663475722' //ID of the Twitch follows channel
+    },
+
+    links: {
+      HandlerInvite: 'https://www.youtube.com/watch?v=dUcVXUSFei4',
+      website: 'www.notlive.com',
+      GenIcon: 'https://thumbs.gfycat.com/BlondBlankKatydid-size_restricted.gif' //G.I.F icon for the generator embed here
+    },
+
+    colors: {               //keep these colors like this too make it look clean but feel free to change if  you feel like it
+      Default: '#ff0000',
+      Red: '#ff0000', 
+      Yellow: '#daff00',  
+      Orange: '#ff8400',
+      Green: '#71fd71',
+      LightGreen: '#5ac18e'
+    },
+
+  colors: {               //keep these colors like this too make it look clean but feel free to change if  you feel like it
+      Default: '#ff0000',
+      Red: '#ff0000', 
+      Yellow: '#daff00',  
+      Orange: '#ff8400',
+      Green: '#71fd71',
+      LightGreen: '#5ac18e'
+    },
+
+    emojis: {
+      success: '<:white_check_mark:853347980824281128>', //Emoji when succeding
+      error: '<a:x:853347975257260033>', //Emoji when wrong
+      loading: '<a:arrows_counterclockwise:853348009235578940>', //Emoji when loading
+      arrow: '<:arrow_right:853348016979181598>', //Arrow Emoji
+      pingpong: '<:ping_pong:853622606199652363>', //Pingpong racket Emoji
+
+      Generator: '<a:Management:853347999086149692>', //Generator emoji for !help embed
+      Twitch: '<:Twitch:853376736695156757>', //Twitch emoji for !help embed
+      Other: '<:Other:853347989699166208>', //Other emoji for !help embed
+      Nitro: '<a:Booster:853683721734193182>', //Nitro emoji for !help embed
+    },
+
+    Token: {
+      Discord: 'yessir',
+    },
+
+    blacklistedUsers: {
+      ID: ['ID', 'ID2', 'ID3'] 
+    }                          
+}
